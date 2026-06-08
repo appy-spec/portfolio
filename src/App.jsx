a37ee964dd1e9b1fc07c65deb803e845e20b55da
appy-spec <rahulpatel120174@gmail.com>
2026-06-08 13:26:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className="text-2xl font-bold bg-black text-white">hi from portfolio</h1>
+    </>
+  )
+}
+
+export default App;
